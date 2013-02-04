85a5966ce7c6b55cd6dada56a52c6f24b99da996
mateusdalforno <>
2013-02-04 12:11:01 -0200

    Criação das interfaces

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author Mateus
+ */
+public class Main {
+    private static TelaPrincipal tela;
+    
+    public static void main (String []args){
+      tela = new TelaPrincipal();
+      tela.setVisible(true);
+    }
+}
